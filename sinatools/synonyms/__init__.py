98e7b0a6fb62db26e5e1dfc64ee25079ef22848f
Tymaa <tymaahasanain@gmail.com>
2024-07-16 19:45:28 +0300

    Uncomment load pkl file

--- sinatools/synonyms/__init__.py
+++ sinatools/synonyms/__init__.py
@@ -11,8 +11,8 @@ with open(file_path, 'rb') as f:
 
 
 synonyms_level3_dict = {}    
-#level3_dict = 'synonyms_level3.pkl'
-#path = downloader.get_appdatadir()
-#file_path = os.path.join(path, level3_dict)
-#with open(file_path, 'rb') as f:
-#    synonyms_level3_dict = pickle.load(f, encoding='utf-8')
+level3_dict = 'synonyms_level3.pkl'
+path = downloader.get_appdatadir()
+file_path = os.path.join(path, level3_dict)
+with open(file_path, 'rb') as f:
+   synonyms_level3_dict = pickle.load(f, encoding='utf-8')
