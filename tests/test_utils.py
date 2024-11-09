33d7c94367da397f93390b34aa6d79d2527f27da
wael <8544289+forzagreen@users.noreply.github.com>
2024-11-09 20:01:49 +0100

    Unit tests: utils.parser

---
+++ tests/test_utils.py
@@ -0,0 +1,41 @@
+import pytest
+
+from sinatools.utils import parser
+
+
+class TestParser:
+    @pytest.mark.parametrize(
+        "input_text, expected_output, kwargs",
+        [
+            ("2023الجو جميلُ", "الجو جميل", {}),
+            ("جَمِيلُ", "جَمِيلُ", {"diacs": False}),
+            ("إِيمَان", "إيمان", {"alif": False}),
+            ("أبريل?! 2024", "أبريل 2024", {"alif": False, "digit": False}),
+        ],
+    )
+    def test_ar_strip(self, input_text, expected_output, kwargs):
+        assert parser.arStrip(input_text, **kwargs) == expected_output
+
+    # TODO: remove_latin keeps spaces. Check if this is the expected behavior.
+    @pytest.mark.parametrize(
+        "input_text, expected_output",
+        [
+            ("miojkdujhvaj1546545spkdpoqfoiehwv", " 1546545 "),
+            (
+                "أصل المسمى «تخطيط موارد المؤسسة» هو Enterprise Resource Planning",
+                "أصل المسمى «تخطيط موارد المؤسسة» هو      ",
+            ),
+        ],
+    )
+    def test_remove_latin(self, input_text, expected_output):
+        assert parser.remove_latin(input_text) == expected_output
+
+    @pytest.mark.parametrize(
+        "input_text, expected_output",
+        [
+            ("te!@#،$%%؟st", "test"),
+            ("{يَا أَيُّهَا الَّذِينَ آمَنُوا لِيَسْتَأْذِنْكُمُ....}", "يَا أَيُّهَا الَّذِينَ آمَنُوا لِيَسْتَأْذِنْكُمُ"),
+        ],
+    )
+    def test_remove_punctuation(self, input_text, expected_output):
+        assert parser.remove_punctuation(input_text) == expected_output
