cd02978a04755a7ab04d765b782a7e664ce75e4f
nabila39 <nabilaothman2468@gmail.com>
2025-05-10 12:53:05 +0300

    v-41

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ requirements = [
     'requests',
     # 'regex',
     'pathlib',
-    'torch==1.13.0',
-    'transformers==4.24.0',
-    'torchtext==0.14.0',
-    'torchvision==0.14.0',
+    # 'torch==2.5.1',
+    'transformers==4.47.1',
+    # 'torchtext==0.14.0',
+    'torchvision==0.20.1',
     'seqeval==1.2.2',
     'natsort==7.1.1'
 ]
